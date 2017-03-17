b0b37a2497a68098229142b3d5bdbebff1447e74
Melissa Yu <melissa.y00@gmail.com>
2017-03-17 13:03:42 -0700

    Add filter modal pt 2

--- app/src/main/java/com/melissayu/cp/mynytimes/activities/ArticleActivity.java
+++ app/src/main/java/com/melissayu/cp/mynytimes/activities/ArticleActivity.java
@@ -1,12 +1,12 @@
 package com.melissayu.cp.mynytimes.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
+import com.melissayu.cp.mynytimes.Article;
 import com.melissayu.cp.mynytimes.R;
 
 public class ArticleActivity extends AppCompatActivity {
@@ -18,14 +18,20 @@ public class ArticleActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Article article = (Article) getIntent().getParcelableExtra("article");
+
+        WebView wv = (WebView) findViewById(R.id.wvArticle);
+
+        wv.setWebViewClient(new WebViewClient(){
+            @SuppressWarnings("deprecation")
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+//                return super.shouldOverrideUrlLoading(view, request);
+                view.loadUrl(url);
+                return true;
             }
         });
+        wv.loadUrl(article.getWebUrl());
     }
 
 }
--- app/src/main/java/com/melissayu/cp/mynytimes/activities/SearchActivity.java
+++ app/src/main/java/com/melissayu/cp/mynytimes/activities/SearchActivity.java
@@ -1,19 +1,28 @@
-package com.melissayu.cp.mynytimes;
+package com.melissayu.cp.mynytimes.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
+import com.melissayu.cp.mynytimes.Article;
+import com.melissayu.cp.mynytimes.ArticleArrayAdapter;
+import com.melissayu.cp.mynytimes.FilterFragment;
+import com.melissayu.cp.mynytimes.FilterFragment.FilterDialogListener;
+import com.melissayu.cp.mynytimes.R;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -22,11 +31,16 @@ import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity implements FilterDialogListener {
 
     EditText etQuery;
     GridView gvResults;
     Button btnSearch;
+
+    String sortBy;
+    String beginDate;
+    String newsDesk;
+
 
     ArrayList<Article> articles;
     ArticleArrayAdapter adapter;
@@ -39,7 +53,6 @@ public class SearchActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         setupViews();
-
     }
 
     public void setupViews() {
@@ -49,6 +62,17 @@ public class SearchActivity extends AppCompatActivity {
         articles = new ArrayList<>();
         adapter = new ArticleArrayAdapter(this, articles);
         gvResults.setAdapter(adapter);
+
+        gvResults.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent i = new Intent(getApplicationContext(), ArticleActivity.class);
+                Article article = articles.get(position);
+                i.putExtra("article", article);
+                startActivity(i);
+            }
+        });
     }
 
     @Override
@@ -63,26 +87,51 @@ public class SearchActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+//        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.miFilter:
+                filterClicked();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    public void filterClicked() {
+        FragmentManager fm = getSupportFragmentManager();
+        FilterFragment filterDialogFragment = FilterFragment.newInstance(sortBy, beginDate, newsDesk);
+        filterDialogFragment.show(fm, "fragment_filter");
+
+    }
+
+    @Override
+    public void onFinishFilterDialog(String sortByVal, String beginDateVal, String newsDeskVal) {
+        Toast.makeText(this, "Hi, " + sortByVal, Toast.LENGTH_SHORT).show();
+        sortBy = sortByVal;
+        beginDate = beginDateVal;
+        newsDesk = newsDeskVal;
     }
 
     public void onArticleSearch(View view) {
         String query = etQuery.getText().toString();
 
-//        Toast.makeText(this, "search for: "+query, Toast.LENGTH_LONG).show();
         AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
         String url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
         RequestParams requestParams = new RequestParams();
         requestParams.put("api-key", "28580fe0e09141ceaa0f57d3ab5eb653");
         requestParams.put("q", query);
         requestParams.put("page", 0);
+
+        if (sortBy != null) {
+            requestParams.put("sort", sortBy.toLowerCase());
+        }
+        if (beginDate != null) {
+            requestParams.put("begin_date", beginDate);
+        }
+        if (newsDesk != null) {
+            requestParams.put("fq", newsDesk);
+        }
 
         asyncHttpClient.get(url, requestParams, new JsonHttpResponseHandler(){
             @Override
@@ -103,4 +152,5 @@ public class SearchActivity extends AppCompatActivity {
             }
         });
     }
+
 }
